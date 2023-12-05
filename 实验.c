67eaf29b073135c7842e329d155695b55086de84
FrankAaron <2741218588@qq.com>
2023-12-05 20:05:05 +0800

    判断单词个数

--- 实验.c
+++ 实验.c
@@ -1,22 +1,21 @@
-//实验  	输出一个含有10行的杨辉三角形
+//实验  	输入一个英文句子，统计句子中单词的个数
+//软件2316 朱昱杰
 #include <stdio.h>
-
+#include <stdlib.h>
 int main()
 {
-	int n = 10;
-	int data[30] = { 1 };
-	printf("1\n");//直接打印第一行
-	for (int i = 1; i < n; i++)//从第二行开始
-	{
-		for (int j = i; j > 0; j--)//从后往前填，避免上一行的数据在使用前就被覆盖
-		{
-			data[j] += data[j - 1];
-		}
-		for (int j = 0; j <= i; j++)
-		{
-			printf("%d ", data[j]);
-		}
-		printf("\n");
-	}
-	return 0;
+    char ch;
+    int sum = 0, word = 0;
+    while ((ch = getchar()) != '\n')
+    {
+        if (ch == ' ')
+            word = 0;
+        else if (word == 0)
+        {
+            word = 1;
+            ++sum;
+        }
+    }
+    printf("%d", sum);
+    return 0;
 }
