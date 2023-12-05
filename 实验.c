5890a06d62f5e47e4e566bf025f68e839568a6e7
FrankAaron <2741218588@qq.com>
2023-12-05 10:17:02 +0800

    Update 实验.c

--- 实验.c
+++ 实验.c
@@ -1,17 +1,23 @@
-//软件2316朱昱杰
-#include <stdio.h>
-#include <math.h>
+//实验	有一个已经排好序的数组，要求输入一个数后，按原来排序的规律将它插入数组中
+
+// 软件2316 朱昱杰
+#include"stdio.h"
 
 int main() {
-    int arr[256];
-    for (int i = 1; i < 1000; i++) {
-        for (int j = 0; j < i; j++) {
-            if (i % j == 0) {
-                int f = 0;
-                arr[f] = j;
-                f++;
-            }
-        }
-    }
-    return 0;
+	int arr[8] = { 1,2,3,4,5,7,8 };
+	int m;
+	scanf_s("%d", &m);
+	for (int i = 0; i < 8; i++) {
+		if (arr[i] >= m) {
+			for (int j = 7; j > i; j--) {
+				arr[j] = arr[j - 1];
+			}
+			arr[i] = m;
+			break;
+		}
+	}
+	for (int i = 0; i < 8; i++) {
+		printf("%d ", arr[i]);
+	}
+	return 0;
 }
